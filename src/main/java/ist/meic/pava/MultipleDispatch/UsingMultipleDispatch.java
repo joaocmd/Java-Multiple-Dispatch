d8b5f8e51b3f1acb3e6874d9a0f32ffd4bf51990
João David <joaodavid@tecnico.ulisboa.pt>
2021-03-28 17:52:51 +0100

    Copy Lectures' starting point

--- src/main/java/ist/meic/pava/MultipleDispatch/UsingMultipleDispatch.java
+++ src/main/java/ist/meic/pava/MultipleDispatch/UsingMultipleDispatch.java
@@ -1,8 +1,30 @@
 package ist.meic.pava.MultipleDispatch;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class UsingMultipleDispatch {
     static Object invoke(Object receiver, String name, Object... args) {
-        // TODO
-        return null;
+        try {
+            Method method = bestMethod(receiver.getClass(), name, args.getClass());
+            return method.invoke(receiver, args);
+        } catch (IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    static Method bestMethod(Class type, String name, Class argType) throws NoSuchMethodException {
+        try {
+            return type.getMethod(name, argType);
+        } catch (NoSuchMethodException e) {
+            if (argType == Object.class) {
+                throw e;
+            } else {
+                return bestMethod(type, name, argType.getSuperclass());
+            }
+        }
     }
 }
