cd8c9703d62fcb6004781485527be951b32c84a4
André Breda <andrebreda@tecnico.ulisboa.pt>
2021-03-30 21:01:37 +0100

    Refactor extended version to comply with all base version rules

--- src/main/java/ist/meic/pava/UsingMultipleDispatchExtended/UsingMultipleDispatch.java
+++ src/main/java/ist/meic/pava/UsingMultipleDispatchExtended/UsingMultipleDispatch.java
@@ -1,47 +1,44 @@
 package ist.meic.pava.UsingMultipleDispatchExtended;
 
-import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Array;
 import java.lang.reflect.Method;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
+
+import ist.meic.pava.MultipleDispatch.MethodSelector;
+import ist.meic.pava.MultipleDispatch.PartialComparator;
+import ist.meic.pava.MultipleDispatch.PartialOrdering;
+import ist.meic.pava.MultipleDispatch.TypeSpecificityComparator;
 
 public class UsingMultipleDispatch {
+    private static MethodSelector methodSelector = new MethodSelector(new VarargsAwareMethodComparator(),
+            new VarargsAwareCandidateMethodFilterFactory());
+
     /**
-     * Invokes the method with name and args of the receiver. Implements dynamic dispatch for the arguments.
-     * Throws RunTimeException on illegal access, missing method, or exception of the invoked method.
-     * @param receiver  the receiver object, that is, the object that contains the method
-     * @param name      the name of the method
-     * @param args      the arguments to pass to the method
-     * @return          the object returned by the method called
+     * Invokes the method with name and args of the receiver. Implements dynamic
+     * dispatch for the arguments. Throws RuntimeException on illegal access,
+     * missing method, or exception of the invoked method.
+     *
+     * @param receiver the receiver object, that is, the object that contains the
+     *                 method
+     * @param name     the name of the method
+     * @param args     the arguments to pass to the method
+     * @return the object returned by the method called
      */
     public static Object invoke(Object receiver, String name, Object... args) {
         try {
-            // Get class type of each Object in args
-            final Class<?>[] argTypes = getArgTypes(args);
-
-            Method bestMethod = Arrays.stream(receiver.getClass().getMethods())
-                .filter(method -> method.getName().equals(name))
-                .filter(method -> method.getParameterCount() == args.length
-                        || (method.isVarArgs() && args.length >= method.getParameterCount() - 1))
-                .map(method -> CandidateMethod.create(method, argTypes))
-                .filter(Objects::nonNull)
-                .min(new CandidateMethodComparator())
-                .map(xmethod -> xmethod.method)
-                .orElseThrow(() -> new NoSuchMethodException(buildNoSuchMethodExceptionMessage(receiver.getClass(), argTypes)));
-
-            return bestMethod.invoke(receiver, evaluateArguments(bestMethod, argTypes, args));
-        } catch (IllegalAccessException
-                | InvocationTargetException
-                | NoSuchMethodException e) {
-            e.printStackTrace();
+            Method method = methodSelector.selectMethod(receiver, name, args);
+            return method.invoke(receiver, evaluateArguments(method, args));
+        } catch (NoSuchMethodException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static boolean shouldBuildVarargsArray(Method method, Class<?>[] argTypes) {
-        // see https://docs.oracle.com/javase/specs/jls/se7/html/jls-15.html#jls-15.12.4.2
-        int k = argTypes.length;
+    private static boolean shouldBuildVarargsArray(Method method, Object[] args) {
+        // see
+        // https://docs.oracle.com/javase/specs/jls/se7/html/jls-15.html#jls-15.12.4.2
+        int k = args.length;
         int n = method.getParameterCount();
 
         if (k != n) {
@@ -49,25 +46,26 @@ public class UsingMultipleDispatch {
         }
 
         // k == n
-        Class<?>[] paramTypes = method.getParameterTypes();
-        return !paramTypes[paramTypes.length - 1].isAssignableFrom(argTypes[argTypes.length - 1]);
+        Class<?> lastParamType = method.getParameterTypes()[n - 1];
+        Class<?> lastArgType = args[k - 1].getClass();
+        return !lastParamType.isAssignableFrom(lastArgType);
     }
 
-    private static Object[] evaluateArguments(Method method, Class<?>[] argTypes, Object... args) throws NegativeArraySizeException {
-        if (method.isVarArgs() && shouldBuildVarargsArray(method, argTypes)) {
+    private static Object[] evaluateArguments(Method method, Object[] args) {
+        if (method.isVarArgs() && shouldBuildVarargsArray(method, args)) {
             int nonVarargsCount = method.getParameterCount() - 1;
             int varargsCount = args.length - nonVarargsCount;
 
             Class<?> varargsType = method.getParameterTypes()[nonVarargsCount];
             assert varargsType.isArray();
-            Object[] varargs = (Object[]) Array.newInstance(varargsType.getComponentType(), varargsCount);
+            Object varargsArray = Array.newInstance(varargsType.getComponentType(), varargsCount);
             if (varargsCount != 0) {
-                System.arraycopy(args, nonVarargsCount, varargs, 0, varargsCount);
+                System.arraycopy(args, nonVarargsCount, varargsArray, 0, varargsCount);
             }
 
             Object[] newargs = new Object[method.getParameterCount()];
             System.arraycopy(args, 0, newargs, 0, nonVarargsCount);
-            newargs[nonVarargsCount] = varargs;
+            newargs[nonVarargsCount] = varargsArray;
 
             return newargs;
         }
@@ -75,107 +73,105 @@ public class UsingMultipleDispatch {
         return args;
     }
 
-    private static Class<?>[] getArgTypes(Object... args) {
-        List<Class<?>> argTypesList = Arrays.stream(args).map(Object::getClass).collect(Collectors.toList());
-        Class<?>[] argTypes = new Class[argTypesList.size()];
-        argTypes = argTypesList.toArray(argTypes);
-        return argTypes;
-    }
+    private static class VarargsAwareMethodComparator implements PartialComparator<Method> {
+        private static TypeSpecificityComparator typeSpecificityComparator = new TypeSpecificityComparator();
 
-    private static String buildNoSuchMethodExceptionMessage(Class<?> receiverType, Class<?>[] argTypes) {
-        return receiverType.getName() +
-                '(' +
-                Arrays.stream(argTypes)
-                        .map(Class::getName)
-                        .collect(Collectors.joining(", ")) +
-                ')';
-    }
-
-    private static class CandidateMethodComparator implements Comparator<CandidateMethod> {
-        public int compare(CandidateMethod a, CandidateMethod b) {
-            int comp = Integer.compare(a.upcastCount(), b.upcastCount());
-            if (comp != 0) {
-                return comp;
+        public PartialOrdering compare(Method lhs, Method rhs) {
+            if (lhs == rhs) {
+                return PartialOrdering.EQUAL;
             }
 
-            for (int i = 0; i < a.upcasts.length && i < b.upcasts.length; i++) {
-                comp = Integer.compare(a.upcasts[i], b.upcasts[i]);
-                if (comp != 0) {
-                    return comp;
+            // if the declaring class of lhs is a subtype of the declaring class of rhs, then lhs is less specific than rhs
+            PartialOrdering receiverPartialOrd = typeSpecificityComparator.compare(lhs.getDeclaringClass(), rhs.getDeclaringClass());
+            if (receiverPartialOrd != PartialOrdering.UNCOMPARABLE && receiverPartialOrd != PartialOrdering.EQUAL) {
+                return receiverPartialOrd;
+            }
+
+            Class<?>[] lhsParamTypes = lhs.getParameterTypes();
+            Class<?>[] rhsParamTypes = rhs.getParameterTypes();
+
+            for (int i = 0; i < lhsParamTypes.length && i < rhsParamTypes.length; i++) {
+                PartialOrdering partialOrd = typeSpecificityComparator.compare(lhsParamTypes[i], rhsParamTypes[i]);
+
+                if (partialOrd != PartialOrdering.UNCOMPARABLE && partialOrd != PartialOrdering.EQUAL) {
+                    return partialOrd;
                 }
             }
 
-            // indirect way of preferring methods without variadic arguments
-            comp = Integer.compare(a.method.getParameterCount(), b.method.getParameterCount());
-            if (comp != 0) {
-                return comp;
+            // if lhs accepts less (non-varargs) parameters than rhs, then lhs is less specific
+            int lhsNormalParamCount = lhsParamTypes.length;
+            int rhsNormalParamCount = rhsParamTypes.length;
+            if (lhs.isVarArgs()) lhsNormalParamCount--;
+            if (rhs.isVarArgs()) rhsNormalParamCount--;
+            int comp = Integer.compare(lhsNormalParamCount, rhsNormalParamCount);
+            if (comp < 0) {
+                return PartialOrdering.LESS;
+            } else if (comp > 0) {
+                return PartialOrdering.GREATER;
             }
 
-            // we really can't do any better, just enforce any total order from here
-            return a.method.toString().compareTo(b.method.toString());
+            // if lhs accepts all arguments that rhs accepts plus varargs, lhs is less specific
+            if (lhs.isVarArgs() && !rhs.isVarArgs()) {
+                return PartialOrdering.LESS;
+            } else if (!lhs.isVarArgs() && rhs.isVarArgs()) {
+                return PartialOrdering.GREATER;
+            }
+
+            return PartialOrdering.UNCOMPARABLE;
         }
     }
 
-    private static class CandidateMethod {
-        public Method method;
-        public int[] upcasts;
+    private static class VarargsAwareCandidateMethodFilterFactory implements MethodSelector.MethodFilterFactory {
+        public Predicate<Method> build(Object receiver, String name, Object[] args) {
+            Class<?>[] argTypes = MethodSelector.getArgTypes(args);
 
-        private CandidateMethod(Method method, int[] upcasts) {
-            this.method = method;
-            this.upcasts = upcasts;
-        }
-
-        static CandidateMethod create(Method method, Class<?>[] argTypes) {
-            int[] upcasts = new int[argTypes.length];
-            Class<?>[] paramTypes = method.getParameterTypes();
-
-            // match non-varargs arguments
-            int limit = method.isVarArgs() ? paramTypes.length - 1 : paramTypes.length;
-            for (int i = 0; i < limit; i++) {
-                Class<?> argType = argTypes[i];
-                Class<?> paramType = paramTypes[i];
-                while (argType != paramType && argType != Object.class) {
-                    argType = argType.getSuperclass();
-                    upcasts[i]++;
+            return method -> {
+                if (!method.getName().equals(name)) {
+                    return false;
                 }
 
-                if (argType != paramType) {
-                    // argType is Object and they are incompatible
-                    return null;
+                Class<?>[] paramTypes = method.getParameterTypes();
+
+                if ((method.isVarArgs() && paramTypes.length - 1 > argTypes.length)
+                        || (!method.isVarArgs() && paramTypes.length != argTypes.length)) {
+                    // normal case: argument and parameter count mismatch
+                    // if varargs: not enough non-vararg arguments
+                    return false;
                 }
-            }
 
-            if (method.isVarArgs()) {
-                int iStart = paramTypes.length - 1; // index of first vararg argument
-                Class<?> paramType = paramTypes[paramTypes.length - 1];
-                assert paramType.isArray();
-                paramType = paramType.getComponentType();
-
-                for (int i = iStart; i < argTypes.length; i++) {
-                    Class<?> argType = argTypes[i];
-                    while (argType != paramType && argType != Object.class) {
-                        argType = argType.getSuperclass();
-                        upcasts[i]++;
-                    }
-
-                    if (argType != paramType) {
-                        // argType is Object and they are incompatible
-                        return null;
+                // Check regular argument compatibility
+                int regularArgCount = method.isVarArgs() ? paramTypes.length - 1 : paramTypes.length;
+                for (int i = 0; i < regularArgCount; i++) {
+                    if (!paramTypes[i].isAssignableFrom(argTypes[i])) {
+                        return false;
                     }
                 }
-            }
 
-            return new CandidateMethod(method, upcasts);
-        }
+                // Check varargs compatibility
+                if (method.isVarArgs()) {
+                    int varargFirstIndex = paramTypes.length - 1;
 
-        public int upcastCount() {
-            return Arrays.stream(this.upcasts)
-                .reduce(0, Integer::sum);
-        }
+                    if (varargFirstIndex == argTypes.length) {
+                        // varargs method with no varargs supplied
+                        return true;
+                    }
 
-        @Override
-        public String toString() {
-            return this.method.toString();
+                    if (paramTypes.length == argTypes.length
+                            && paramTypes[varargFirstIndex].isAssignableFrom(argTypes[varargFirstIndex])) {
+                        // varargs method with args array supplied
+                        return true;
+                    }
+
+                    Class<?> varargType = paramTypes[varargFirstIndex].getComponentType();
+                    for (int i = varargFirstIndex; i < argTypes.length; i++) {
+                        if (!varargType.isAssignableFrom(argTypes[i])) {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            };
         }
     }
 }
