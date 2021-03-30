a39d8f569c0552b3ec577f107f6a57cf23e44bc4
André Breda <andrebreda@tecnico.ulisboa.pt>
2021-03-30 14:47:36 +0100

    Use extended method finding algorithm for base version

--- src/main/java/ist/meic/pava/MultipleDispatch/UsingMultipleDispatch.java
+++ src/main/java/ist/meic/pava/MultipleDispatch/UsingMultipleDispatch.java
@@ -6,74 +6,110 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class UsingMultipleDispatch {
-
     /**
-     * Invokes the method with name and args of the receiver. Implements dynamic
-     * dispatch for the arguments. Throws RunTimeException on illegal access,
-     * missing method, or exception of the invoked method.
-     * @param receiver    the receiver object, that is, the object that contains the
-     *         method
-     * @param name            the name of the method
-     * @param args            the arguments to pass to the method
-     * @return                    the object returned by the method called
+     * Invokes the method with name and args of the receiver. Implements dynamic dispatch for the arguments.
+     * Throws RunTimeException on illegal access, missing method, or exception of the invoked method.
+     * @param receiver  the receiver object, that is, the object that contains the method
+     * @param name      the name of the method
+     * @param args      the arguments to pass to the method
+     * @return          the object returned by the method called
      */
     public static Object invoke(Object receiver, String name, Object... args) {
         try {
             // Get class type of each Object in args
-            List<Class<?>> argTypesList = Arrays.stream(args)
-                .map(Object::getClass)
-                .collect(Collectors.toList());
-            Class<?>[] argTypes = new Class[argTypesList.size()];
-            argTypes = argTypesList.toArray(argTypes);
+            final Class<?>[] argTypes = getArgTypes(args);
 
-            Method method = bestMethod(receiver.getClass(), name, argTypes);
-            return method.invoke(receiver, args);
-        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            Method bestMethod = Arrays.stream(receiver.getClass().getMethods())
+                .filter(method -> method.getName().equals(name))
+                .filter(method -> method.getParameterCount() == args.length)
+                .map(method -> CandidateMethod.create(method, argTypes))
+                .filter(Objects::nonNull)
+                .min(new CandidateMethodComparator())
+                .map(xmethod -> xmethod.method)
+                .orElseThrow(() -> new NoSuchMethodException(buildNoSuchMethodExceptionMessage(receiver.getClass(), argTypes)));
+
+            return bestMethod.invoke(receiver, args);
+        } catch (IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
 
-    /**
-     * Implements a Breadth-First-Search for finding and returning the most
-     * specific method for the given argument types.
-     * @param receiverType            the type of the receiver
-     * @param name            the name of the method to find
-     * @param argTypes    an Array with the ordered argument types
-     * @return                    the specified Method
-     * @throws NoSuchMethodException if no implementation was found
-     */
-    static Method bestMethod(Class<?> receiverType, String name,
-                                                     Class<?>[] argTypes) throws NoSuchMethodException {
-        // BFS over argument types
-        Queue<Class<?>[]> queue = new ArrayDeque<>();
-        queue.add(argTypes);
+    private static Class<?>[] getArgTypes(Object... args) {
+        List<Class<?>> argTypesList = Arrays.stream(args).map(Object::getClass).collect(Collectors.toList());
+        Class<?>[] argTypes = new Class[argTypesList.size()];
+        argTypes = argTypesList.toArray(argTypes);
+        return argTypes;
+    }
 
-        while (queue.peek() != null) {
-            Class<?>[] currArgTypes = queue.poll();
+    private static String buildNoSuchMethodExceptionMessage(Class<?> receiverType, Class<?>[] argTypes) {
+        return receiverType.getName() +
+                '(' +
+                Arrays.stream(argTypes)
+                        .map(Class::getName)
+                        .collect(Collectors.joining(", ")) +
+                ')';
+    }
 
-            try {
-                // this is the most specialized method available, use it
-                return receiverType.getMethod(name, currArgTypes);
-            } catch (NoSuchMethodException e) {
-                // ignored
+    private static class CandidateMethodComparator implements Comparator<CandidateMethod> {
+        public int compare(CandidateMethod a, CandidateMethod b) {
+            int comp = Integer.compare(a.upcastCount(), b.upcastCount());
+            if (comp != 0) {
+                return comp;
             }
 
-            // enqueue more general argument types
-            for (int i = 0; i < currArgTypes.length; i++) {
-                if (currArgTypes[i] != Object.class) {
-                    Class<?>[] moreGeneralArgTypes = currArgTypes.clone();
-                    moreGeneralArgTypes[i] = currArgTypes[i].getSuperclass();
-                    queue.add(moreGeneralArgTypes);
+            for (int i = 0; i < a.upcasts.length && i < b.upcasts.length; i++) {
+                comp = Integer.compare(a.upcasts[i], b.upcasts[i]);
+                if (comp != 0) {
+                    return comp;
                 }
             }
+
+            // we really can't do any better, just enforce any total order from here
+            return a.method.toString().compareTo(b.method.toString());
+        }
+    }
+
+    private static class CandidateMethod {
+        public Method method;
+        public int[] upcasts;
+
+        private CandidateMethod(Method method, int[] upcasts) {
+            this.method = method;
+            this.upcasts = upcasts;
         }
 
-        String msg = receiverType.getName() + '(' +
-                                 Arrays.stream(argTypes)
-                                         .map(Class::getName)
-                                         .collect(Collectors.joining(", ")) +
-                                 ')';
-        throw new NoSuchMethodException(msg);
+        static CandidateMethod create(Method method, Class<?>[] argTypes) {
+            int[] upcasts = new int[argTypes.length];
+            Class<?>[] paramTypes = method.getParameterTypes();
+
+            for (int i = 0; i < paramTypes.length; i++) {
+                Class<?> argType = argTypes[i];
+                Class<?> paramType = paramTypes[i];
+                while (argType != paramType && argType != Object.class) {
+                    argType = argType.getSuperclass();
+                    upcasts[i]++;
+                }
+
+                if (argType != paramType) {
+                    // argType is Object and they are incompatible
+                    return null;
+                }
+            }
+
+            return new CandidateMethod(method, upcasts);
+        }
+
+        public int upcastCount() {
+            return Arrays.stream(this.upcasts)
+                .reduce(0, Integer::sum);
+        }
+
+        @Override
+        public String toString() {
+            return this.method.toString();
+        }
     }
 }
